0261ef1098aec0e628066544c26942212ff42317
Txus Ordorika <txusinho@protonmail.ch>
2022-02-12 23:57:41 +0100

    Add main computing function

---
+++ test/unit/main.spec.js
@@ -0,0 +1,36 @@
+const compute = require("../../src/main");
+
+describe("computeDigits", function () {
+    context("any negative number on the list", () => {
+        it("keeps working", () => {
+            const data = [ -23, 65 ];
+            const result = compute(data);
+            expect(result).to.equal(8);
+        });
+    });
+    context("all number are integer positive", () => {
+        it("returns the sum", () => {
+            const data = [ 23, 65, 123, 343, 123, 346, 12 ];
+            const result = compute(data);
+            expect(result).to.equal(9);
+        });
+    });  
+
+    context("the sum of all digits is a number with more than one digit", () => {
+        context("the sum is 2 digits length", () => {
+            it("returns the sum correctly", () => {
+                const data = [ 33, 123, 343, 123, 346, 12 ];
+                const result = compute(data);
+                expect(result).to.equal(8);
+            });
+        });
+
+        context("works for any length", () => {
+            it("returns the sum correctly", () => {
+                const data = [111111111111, 111111111111, 111111111111, 111111111111, 1111111111111, 111111111111, 111111111111, 111111111111, 111111111111];
+                const result = compute(data);
+                expect(result).to.equal(1);
+            });
+        });
+    });
+});
